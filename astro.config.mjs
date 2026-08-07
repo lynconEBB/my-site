01b6f89b274fe63871f475bda1ddd36b3889d777
Lyncon Baez <lyncon.baez@pti.org.br>
2026-08-07 14:56:59 -0300

    Adding custom font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    fonts: [{
+        provider: fontProviders.local(),
+        name: "Oxanium",
+        cssVariable: "--font-oxanium",
+        options: {
+            variants:[{
+                src: ["./src/assets/fonts/oxanium.ttf"],
+                weight: "200 8000",
+                style: "normal",
+            }]
+        }
+    }]
+});
