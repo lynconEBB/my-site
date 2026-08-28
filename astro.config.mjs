79c3e378ee0867ec3c99a75114560306662d1999
Lyncon Baez <lyncon.baez@pti.org.br>
2026-08-28 08:33:06 -0300

    Adding cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,34 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-    fonts: [{
-        provider: fontProviders.local(),
-        name: "Oxanium",
-        cssVariable: "--font-oxanium",
-        options: {
-            variants:[{
-                src: ["./src/assets/fonts/oxanium.ttf"],
-                weight: "200 800",
-                style: "normal",
-            }]
-        }
-    },
-    {
-        provider: fontProviders.local(),
-        name: "Anton",
-        cssVariable: "--font-anton",
-        options: {
-            variants:[{
-                src: ["./src/assets/fonts/anton.ttf"],
-                weight: "normal",
-                style: "normal",
-            }]
-        }
-    }]
+  fonts: [{
+      provider: fontProviders.local(),
+      name: "Oxanium",
+      cssVariable: "--font-oxanium",
+      options: {
+          variants:[{
+              src: ["./src/assets/fonts/oxanium.ttf"],
+              weight: "200 800",
+              style: "normal",
+          }]
+      }
+  },
+  {
+      provider: fontProviders.local(),
+      name: "Anton",
+      cssVariable: "--font-anton",
+      options: {
+          variants:[{
+              src: ["./src/assets/fonts/anton.ttf"],
+              weight: "normal",
+              style: "normal",
+          }]
+      }
+  }],
 
+  adapter: cloudflare()
 });
